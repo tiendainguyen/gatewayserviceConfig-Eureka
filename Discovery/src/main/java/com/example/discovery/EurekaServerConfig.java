ff76f5d3160cbc71e4636b4cd75b21505f752983
tiendainguyen <daint.dev@gmail.com>
2023-03-27 20:40:10 +0700

    update: readme

--- Discovery/src/main/java/com/example/discovery/EurekaServerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.discovery;
-
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableEurekaServer
-public class EurekaServerConfig {
-}
